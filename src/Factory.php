10e92dcd3f7f212b57fc0599809cae0c838591ef
TONYLABS <tony.wang@tonylabs.com>
2022-06-05 00:39:09 +0800

    Update Factory.php

--- src/Factory.php
+++ src/Factory.php
@@ -4,23 +4,13 @@ namespace TONYLABS\Shopify;
 
 class Factory
 {
-    public static function fromConfig(): Shopify
+    public static function env(): Shopify
     {
-        return new Shopify(
-            config('shopify.credentials.api_key'),
-            config('shopify.credentials.api_token'),
-            config('shopify.credentials.domain'),
-            config('shopify.credentials.api_version'),
-        );
+        return new Shopify(env('SHOPIFY_API_KEY'), env('SHOPIFY_API_TOKEN'), env('SHOPIFY_API_VERSION'), env('SHOPIFY_DOMAIN'));
     }
 
-    public static function fromArray(array $data): Shopify
+    public static function config(array $config): Shopify
     {
-        return new Shopify(
-            $data['api_key'],
-            $data['api_token'],
-            $data['domain'],
-            $data['api_version']
-        );
+        return new Shopify($config['api_key'], $config['api_token'], $config['api_version'], $config['domain']);
     }
 }
